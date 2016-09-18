3ad72fe1de5f8eb065721de42e41e16162ccc619
Alexey Papulovskiy <a@c17.nl>
2016-09-18 13:36:27 +0200

    EPI: 53, variation with swapping only until max significant bit

--- epi/53.py
+++ epi/53.py
@@ -1,6 +1,11 @@
 def rev(num):
     result = 0
-    limit = 32
+    limit = 31
+    comp = 1 << limit
+    while not num & comp:
+        comp = comp >> 1
+        limit -= 1
+    limit += 1
     for x in range(0, limit):
         result = result | ((1 if num & (1 << x) else 0) << (limit - 1 - x))
     return result
